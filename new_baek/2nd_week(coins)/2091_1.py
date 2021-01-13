099b21facce1229253122415a299d8bae18616fe
Junha Moon <ririro93@gmail.com>
2021-01-13 23:40:25 +0900

    01-13 solves

--- new_baek/2nd_week(coins)/2091_1.py
+++ new_baek/2nd_week(coins)/2091_1.py
@@ -1,1 +1,15 @@
-# 다시 정리해서 낼 해보기
+# 어떻게든 해볼라 했는데 이렇게 조건 하나하나 주다간 끝이 없을듯
+# use as manay coins as possible 
+
+from sys import stdin
+
+stdin = open('input.txt')
+input = stdin.readline
+
+# inputs
+P, C1, C2, C3, C4 = map(int, input().split())
+C = [C1, C2, C3, C4] # [5, 3, 1, 2]
+temp = C[:]
+
+# init
+coins = [1, 5, 10, 25]
