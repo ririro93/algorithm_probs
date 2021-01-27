97479d97039254c3c47dad6378e5b28dc3de46a4
Junha Moon <ririro93@gmail.com>
2021-01-27 21:39:58 +0900

    01-27 solves

--- leetcode/daily_problems/1680_Concatenation_of_Consecutive_Binary_Numbers.py
+++ leetcode/daily_problems/1680_Concatenation_of_Consecutive_Binary_Numbers.py
@@ -1,12 +1,11 @@
 class Solution:
     def concatenatedBinary(self, n: int) -> int:
         result = 1
-        x = int(log(n, 2))
 
         # f(n) = (2**x) * f(n-1) + n
         for i in range(2, n+1):
             x = int(log(i, 2)) + 1
             result = (2**x) * result + i
-            result %= int(1e9 + 7)
+            result %= int(1e9 + 7) # 10 ** 9 + 7 이 int로 바꾸는 작업이 없어서 더 좋을듯
         
         return result
