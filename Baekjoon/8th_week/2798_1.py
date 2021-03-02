6fe1e7b25e734108aa8beed2e7dc6e2624c8e907
Junha Moon <ririro93@gmail.com>
2021-03-02 17:07:56 +0900

    03-02 solves

--- Baekjoon/8th_week/2798_1.py
+++ Baekjoon/8th_week/2798_1.py
@@ -2,3 +2,30 @@ from sys import stdin
 
 # stdin = open('input.txt', 'r')
 input = stdin.readline
+
+# functions
+def solve(N, M, K, tmp):
+    global total
+    if K == 3:
+        total = max(total, tmp)
+    else:
+        for i in range(N):
+            if not checked[i]:
+                if tmp + cards[i] > M:
+                    continue
+                else:
+                    checked[i] = True
+                    solve(N, M, K+1, tmp+cards[i])
+                    checked[i] = False
+
+# inputs
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+# init
+checked = [False] * N
+total = 0
+
+# exec
+solve(N, M, 0, 0)
+print(total)
